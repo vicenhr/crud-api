3cded2bd58b102f1572e27cac96c1d605e2cd720
Vicente Hernandez <vicxnte.hr.17@gmail.com>
2026-07-16 12:48:32 -0600

    Stage 1: root and health endopoints

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Middleware para poder leer JSON en el body de las requests (lo necesitarás en Stage 3)
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.json({
+    name: "Task API",
+    version: "1.0",
+    endpoints: ["/tasks"]
+  });
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: "ok" });
 });
 
 app.listen(port, () => {
